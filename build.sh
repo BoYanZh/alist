9eb8a5f42ed0548efff091d55b30d530650c08bd
BoYanZh <boyanzh233@gmail.com>
2022-12-12 22:02:49 +0800

    ci: build linux/arm64 on dev

--- build.sh
+++ build.sh
@@ -43,12 +43,11 @@ FetchWebRelease() {
 
 BuildDev() {
   rm -rf .git/
-  xgo -targets=linux/amd64,windows/amd64,darwin/amd64 -out "$appName" -ldflags="$ldflags" -tags=jsoniter .
+  xgo -targets=linux/arm64 -out "$appName" -ldflags="$ldflags" -tags=jsoniter .
   mkdir -p "dist"
   mv alist-* dist
   cd dist
   upx -9 ./alist-linux*
-  upx -9 ./alist-windows*
   find . -type f -print0 | xargs -0 md5sum >md5.txt
   cat md5.txt
 }
